542f564a9145ca14b097d7f1ff18592395d14850
Jordan Powers <jordanpowers1227@gmail.com>
2025-01-07 22:44:41 -0800

    Add MoPrefs

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.molib.prefs.MoPrefs;
 
 public class Robot extends TimedRobot {
     private Command m_autonomousCommand;
@@ -15,6 +16,11 @@ public class Robot extends TimedRobot {
 
     public Robot() {
         m_robotContainer = new RobotContainer();
+    }
+
+    @Override
+    public void robotInit() {
+        MoPrefs.cleanUpPrefs();
     }
 
     @Override
