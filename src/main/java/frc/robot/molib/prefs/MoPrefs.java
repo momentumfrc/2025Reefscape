b7a9978fa3124a54432d2f4f020b1003b491e1cd
Momentum Robotics <momentumrobotics@gmail.com>
2025-01-28 19:57:05 -0800

    Added code for elevator encoders

--- src/main/java/frc/robot/molib/prefs/MoPrefs.java
+++ src/main/java/frc/robot/molib/prefs/MoPrefs.java
@@ -42,6 +42,23 @@ public class MoPrefs {
     public static final AngleUnitPref swerveRLZero = rotationsPref("SWRV Zero RL", Units.Rotations.of(0.895));
     public static final AngleUnitPref swerveRRZero = rotationsPref("SWRV Zero RR", Units.Rotations.of(0.968));
 
+    public static final UnitPref<PerUnit<DimensionlessUnit, AngleUnit>> elevatorEncoderScale =
+            encoderTicksPerRotationPref("Elevator Encoder Scale", MoUnits.EncoderTicksPerRotation.ofNative(144));
+    public static final UnitPref<PerUnit<DimensionlessUnit, AngleUnit>> elevatorAbsEncoderScale =
+            encoderTicksPerRotationPref(
+                    "Elevator Absolute Encoder Scale", MoUnits.EncoderTicksPerRotation.ofNative(2.4));
+    public static final UnitPref<PerUnit<DimensionlessUnit, AngleUnit>> wristEncoderScale =
+            encoderTicksPerRotationPref("Wrist Encoder Scale", MoUnits.EncoderTicksPerRotation.ofNative(1));
+
+    public static final UnitPref<AngleUnit> elevatorAbsZero =
+            rotationsPref("Shoulder Absolute Zero", Units.Rotations.of(0));
+    public static final UnitPref<AngleUnit> wristAbsZero = rotationsPref("Wrist Absolute Zero", Units.Rotations.of(0));
+
+    public static final UnitPref<AngleUnit> elevatorMaxExtension =
+            rotationsPref("Wrist Absolute Zero", Units.Degrees.of(0));
+    public static final UnitPref<AngleUnit> wristMaxExtension =
+            rotationsPref("Wrist Absolute Zero", Units.Degrees.of(0));
+
     public static final UnitPref<VoltageUnit> endEffectorPower = voltsPref("End Effector Power", Units.Volts.of(5));
 
     /**
--- src/main/java/frc/robot/subsystem/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystem/ElevatorSubsystem.java
@@ -6,12 +6,15 @@ import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SoftLimitConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.units.Units;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-// import frc.robot.molib.encoder.MoRotationEncoder;
+import frc.robot.molib.encoder.MoRotationEncoder;
+// import frc.robot.molib.pid.MoSparkMaxElevatorPID;
 import frc.robot.molib.prefs.MoPrefs;
 import frc.robot.utils.MoUtils;
 
@@ -24,10 +27,22 @@ public class ElevatorSubsystem extends SubsystemBase {
     private final SparkMax elevatorWrist;
     private final VictorSPX endEffector;
 
-    // public final MoRotationEncoder elevatorAbsEncoder;
-    // public final MoRotationEncoder wristAbsEncoder;
-    // public final MoRotationEncoder elevatorRelEncoder;
-    // public final MoRotationEncoder wristRelEncoder;
+    private SparkMaxConfig elevatorAConfig = new SparkMaxConfig();
+    private SparkMaxConfig elevatorBConfig = new SparkMaxConfig();
+    private SparkMaxConfig wristConfig = new SparkMaxConfig();
+    private SoftLimitConfig elevatorLimitConfig = new SoftLimitConfig();
+    private SoftLimitConfig wristLimitConfig = new SoftLimitConfig();
+
+    public final MoRotationEncoder elevatorAbsEncoder;
+    public final MoRotationEncoder wristAbsEncoder;
+    public final MoRotationEncoder elevatorRelEncoder;
+    public final MoRotationEncoder wristRelEncoder;
+
+    // TODO:
+    // private final MoSparkMaxElevatorPID elevatorVelocityPid;
+    // private final MoSparkMaxElevatorPID wristVelocityPid;
+    // private final MoSparkMaxElevatorPID elevatorSmartMotionPid;
+    // private final MoSparkMaxElevatorPID wristSmartMotionPid;
 
     public static record ElevatorPosition(Angle elevatorAngle, Angle wristAngle) {}
 
@@ -43,25 +58,66 @@ public class ElevatorSubsystem extends SubsystemBase {
     }
 
     public void configureMotors() {
-        this.elevatorA.configure(
-                new SparkMaxConfig().smartCurrentLimit(ELEVATOR_CURRENT_LIMIT),
-                ResetMode.kResetSafeParameters,
-                PersistMode.kNoPersistParameters);
-        this.elevatorB.configure(
-                new SparkMaxConfig().smartCurrentLimit(ELEVATOR_CURRENT_LIMIT).follow(elevatorA, true),
-                ResetMode.kResetSafeParameters,
-                PersistMode.kNoPersistParameters);
-        this.elevatorWrist.configure(
-                new SparkMaxConfig().smartCurrentLimit(WRIST_CURRENT_LIMIT),
-                ResetMode.kResetSafeParameters,
-                PersistMode.kNoPersistParameters);
+        elevatorLimitConfig.reverseSoftLimit(0).reverseSoftLimitEnabled(true);
+        elevatorAConfig
+                .idleMode(IdleMode.kBrake)
+                .smartCurrentLimit(ELEVATOR_CURRENT_LIMIT)
+                .apply(elevatorLimitConfig);
+        elevatorBConfig.apply(elevatorAConfig).follow(elevatorA, true);
+        wristConfig
+                .idleMode(IdleMode.kBrake)
+                .smartCurrentLimit(WRIST_CURRENT_LIMIT)
+                .apply(wristLimitConfig);
+
+        this.elevatorA.configure(elevatorAConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+        this.elevatorB.configure(elevatorBConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+        this.elevatorWrist.configure(wristConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+
+        MoUtils.setupRelativeEncoder(
+                elevatorRelEncoder,
+                elevatorAbsEncoder.getPosition(),
+                MoPrefs.elevatorAbsZero.get(),
+                MoPrefs.elevatorEncoderScale.get());
+
+        MoUtils.setupRelativeEncoder(
+                wristRelEncoder,
+                wristAbsEncoder.getPosition(),
+                MoPrefs.wristAbsZero.get(),
+                MoPrefs.wristEncoderScale.get());
+
+        elevatorAbsEncoder.setConversionFactor(MoPrefs.elevatorAbsEncoderScale.get());
     }
 
     public ElevatorSubsystem() {
+        super("Elevator");
+
         this.elevatorA = new SparkMax(Constants.ELEVATORA.address(), MotorType.kBrushless);
         this.elevatorB = new SparkMax(Constants.ELEVATORB.address(), MotorType.kBrushless);
         this.elevatorWrist = new SparkMax(Constants.ELEVATOR_WRIST.address(), MotorType.kBrushless);
         this.endEffector = new VictorSPX(Constants.END_EFFECTOR.address());
+
+        elevatorAbsEncoder = MoRotationEncoder.forSparkAbsolute(elevatorA, Units.Rotations);
+        wristAbsEncoder = MoRotationEncoder.forSparkAbsolute(elevatorWrist, Units.Rotations);
+
+        elevatorRelEncoder = MoRotationEncoder.forSparkRelative(elevatorA, Units.Rotations);
+        wristRelEncoder = MoRotationEncoder.forSparkRelative(elevatorWrist, Units.Rotations);
+
+        // Setup listeners for encoder scales and absolute zeros. Use notifyImmediately on zero listeners to set the
+        // values now.  TODO: ask Jordan what this means
+        MoPrefs.elevatorEncoderScale.subscribe(scale -> MoUtils.setupRelativeEncoder(
+                elevatorRelEncoder, elevatorAbsEncoder.getPosition(), MoPrefs.elevatorAbsZero.get(), scale));
+        MoPrefs.elevatorAbsEncoderScale.subscribe(elevatorAbsEncoder::setConversionFactor, true);
+        MoPrefs.wristEncoderScale.subscribe(scale -> MoUtils.setupRelativeEncoder(
+                wristRelEncoder, wristAbsEncoder.getPosition(), MoPrefs.wristAbsZero.get(), scale));
+        MoPrefs.elevatorAbsZero.subscribe(zero -> MoUtils.setupRelativeEncoder(
+                elevatorRelEncoder, elevatorAbsEncoder.getPosition(), zero, MoPrefs.elevatorEncoderScale.get()));
+        MoPrefs.wristAbsZero.subscribe(zero -> MoUtils.setupRelativeEncoder(
+                wristRelEncoder, wristAbsEncoder.getPosition(), zero, MoPrefs.wristEncoderScale.get()));
+
+        MoPrefs.elevatorMaxExtension.subscribe(limit ->
+                elevatorLimitConfig.forwardSoftLimit((float) limit.in(elevatorRelEncoder.getInternalEncoderUnits())));
+        MoPrefs.wristMaxExtension.subscribe(limit ->
+                wristLimitConfig.forwardSoftLimit((float) limit.in(wristRelEncoder.getInternalEncoderUnits())));
     }
 
     public void intakeAlgaeCoralExtake() {
