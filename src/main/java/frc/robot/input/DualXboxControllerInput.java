da108cc137f4fb31518eb0bb21851611b1d7f4bf
Momentum Robotics <momentumrobotics@gmail.com>
2025-02-24 17:13:54 -0800

    Added Option of Dual Xbox Control

---
+++ src/main/java/frc/robot/input/DualXboxControllerInput.java
@@ -0,0 +1,105 @@
+package frc.robot.input;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.Constants;
+import frc.robot.component.ElevatorSetpointManager.ElevatorSetpoint;
+import frc.robot.subsystem.ElevatorSubsystem.ElevatorMovementRequest;
+import frc.robot.utils.Vec2;
+import java.util.Optional;
+
+public class DualXboxControllerInput implements MoInput {
+
+    private XboxController driveController = new XboxController(Constants.JOYSTICK.hidport());
+    private XboxController elevatorController = new XboxController(Constants.XBOX_CONTROLLER_1.hidport());
+
+    private Vec2 moveRequest = new Vec2(0, 0);
+
+    private double throttle = 0;
+
+    private double getThrottle() {
+        if (driveController.getAButtonPressed()) throttle -= .05;
+        if (driveController.getYButtonPressed()) throttle += .05;
+        MathUtil.clamp(throttle, 0, 1);
+        return throttle;
+    }
+
+    @Override
+    public Vec2 getMoveRequest() {
+        moveRequest.update(driveController.getLeftX(), driveController.getLeftY());
+        moveRequest.scale(getThrottle());
+        return moveRequest;
+    }
+
+    @Override
+    public double getTurnRequest() {
+        return -1 * driveController.getRightX() * getThrottle();
+    }
+
+    @Override
+    public boolean getReZeroGyro() {
+        return driveController.getRawButton(7);
+    }
+
+    @Override
+    public boolean getSaveElevatorSetpoint() {
+        return elevatorController.getStartButtonPressed();
+    }
+
+    @Override
+    public ElevatorMovementRequest getElevatorMovementRequest() {
+        return new ElevatorMovementRequest(elevatorController.getLeftY(), elevatorController.getRightY());
+    }
+
+    @Override
+    public Optional<ElevatorSetpoint> getElevatorSetpoints() {
+        double pov = elevatorController.getPOV();
+        if (pov == 0) {
+            if (elevatorController.getYButton()) {
+                return Optional.of(ElevatorSetpoint.L3);
+            } else if (elevatorController.getBButton()) {
+                return Optional.of(ElevatorSetpoint.L2);
+            } else if (elevatorController.getAButton()) {
+                return Optional.of(ElevatorSetpoint.L1);
+            }
+        } else {
+            if (elevatorController.getYButton()) {
+                return Optional.of(ElevatorSetpoint.INTAKE);
+            } else if (elevatorController.getBButton()) {
+                return Optional.of(ElevatorSetpoint.STOW);
+            } else if (elevatorController.getAButton()) {
+                return Optional.of(ElevatorSetpoint.PROCESSOR);
+            }
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean getReZeroElevator() {
+        return elevatorController.getBackButtonPressed();
+    }
+
+    @Override
+    public boolean getRunSysid() {
+        return elevatorController.getStartButton();
+    }
+
+    @Override
+    public boolean getEndEffectorIn() {
+        return elevatorController.getLeftBumperButton();
+    }
+
+    @Override
+    public boolean getEndEffectorOut() {
+        return elevatorController.getRightBumperButton();
+    }
+
+    public boolean getIntake() {
+        return driveController.getRawButton(1);
+    }
+
+    @Override
+    public double getClimberMoveRequest() {
+        return (-1 * elevatorController.getLeftTriggerAxis()) + elevatorController.getRightTriggerAxis();
+    }
+}
